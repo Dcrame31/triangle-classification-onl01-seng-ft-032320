99c8d3c3765447427f92be817f39e3621d723ef4
Diane Crame <dcrame@icloud.com>
2020-04-08 00:55:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,9 @@ class Triangle
   end
   
   def kind
-    if self.side_one == self.side_two && self.side_two == self.side_three = :equilateral
-    elsif
-      self.side_one != self.side_two && self.side_one != self.side_three = :scalene
-      elsif
-      self.side_one == self.side_two || self.side_two == self.side_three = :isosceles
+    self.side_one == self.side_two && self.side_two == self.side_three = :equilateral
+    self.side_one != self.side_two && self.side_one != self.side_three = :scalene
+    self.side_one == self.side_two || self.side_two == self.side_three = :isosceles
       
     end
   end
