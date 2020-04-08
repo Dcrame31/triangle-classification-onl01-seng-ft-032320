2b536709e801f01020bfe2351639ac43883c6b20
Diane Crame <dcrame@icloud.com>
2020-04-08 01:12:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
     elsif
     self.side_two == self.side_three || self.side_two == self.side_one || self.side_one == self.side_three
       return  :isosceles
-      if self.side_one <1 || self.side_two <1 || self.side_three <1
+      if !self.side_one || !self.side_two || !self.side_three
         raise TriangleError
     end
     end
